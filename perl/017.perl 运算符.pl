f2c74ddb8e5d174ffd63216a697af4707bbc9a35
liheng <1044173118@qq.com>
2020-12-10 14:30:07 +0800

    Update 017.perl 运算符.pl

--- perl/017.perl 运算符.pl
+++ perl/017.perl 运算符.pl
@@ -97,5 +97,171 @@ if( $a <= $b ){
 
 print("\n_________________________________比较运算符(字符串)__________________________________________________\n");
 
+=pod
 
+      lt	<
+      gt	>
+      le	<=
+      ge	>=
+      eq	==
+      ne	!=
+      cmp	compareTo
+
+=cut
+
+
+$a = "abc";
+$b = "xyz";
+ 
+print "\$a = $a ，\$b = $b\n";
+ 
+if( $a lt $b ){
+   print "$a lt \$b 返回 true\n";
+}else{
+   print "\$a lt \$b 返回 false\n";
+}
+ 
+if( $a gt $b ){
+   print "\$a gt \$b 返回 true\n";
+}else{
+   print "\$a gt \$b 返回 false\n";
+}
+ 
+if( $a le $b ){
+   print "\$a le \$b 返回 true\n";
+}else{
+   print "\$a le \$b 返回 false\n";
+}
+ 
+if( $a ge $b ){
+   print "\$a ge \$b 返回 true\n";
+}else{
+   print "\$a ge \$b 返回 false\n";
+}
+ 
+if( $a ne $b ){
+   print "\$a ne \$b 返回 true\n";
+}else{
+   print "\$a ne \$b 返回 false\n";
+}
+ 
+$c = $a cmp $b;
+print "\$a cmp \$b 返回 $c\n";
+
+print("\n_________________________________赋值运算符__________________________________________________\n");
+
+=pod
+      =
+      +=
+      -=
+      *=
+      /=
+      %=
+      **=
+=cut
+$a = 10;
+$b = 20;
+ 
+print "\$a = $a ，\$b = $b\n";
+ 
+$c = $a + $b;
+print "赋值后 \$c = $c\n";
+ 
+$c += $a;
+print "\$c = $c ，运算语句 \$c += \$a\n";
+ 
+$c -= $a;
+print "\$c = $c ，运算语句 \$c -= \$a\n";
+ 
+$c *= $a;
+print "\$c = $c ，运算语句 \$c *= \$a\n";
+ 
+$c /= $a;
+print "\$c = $c ，运算语句 \$c /= \$a\n";
+ 
+$c %= $a;
+print "\$c = $c ，运算语句 \$c %= \$a\n";
+ 
+$c = 2;
+$a = 4;
+print "\$a = $a ， \$c = $c\n";
+$c **= $a;
+print "\$c = $c ，运算语句 \$c **= \$a\n";
+
+print("\n_________________________________位运算__________________________________________________\n");
+＝pod
+      #设置 $a = 60，$b = 13，现在以二进制格式表示，它们如下所示：
+      $a = 0011 1100
+      $b = 0000 1101
+
+      $a&$b = 0000 1100
+      $a|$b = 0011 1101
+      $a^$b = 0011 0001
+      ~$a  = 1100 0011
+
+      &     且运算
+      |     或运算
+      ^	异或，存在但不同时存在时，取值 。
+      ~	取反，即0变成1，1变成0。
+      <<    左移运算
+      >>    右移运算
+
+=cut
+
+use integer;
+ 
+$a = 60;
+$b = 13;
+ 
+print "\$a = $a , \$b = $b\n";
+ 
+$c = $a & $b;
+print "\$a & \$b = $c\n";
+ 
+$c = $a | $b;
+print "\$a | \$b = $c\n";
+ 
+$c = $a ^ $b;
+print "\$a ^ \$b = $c\n";
+ 
+$c = ~$a;
+print "~\$a = $c\n";
+ 
+$c = $a << 2;
+print "\$a << 2 = $c\n";
+ 
+$c = $a >> 2;
+print "\$a >> 2 = $c\n";
+
+
+print("\n_________________________________逻辑运算符__________________________________________________\n");
+
+=pod
+      and 相当于java的 &
+      &&
+      or 相当于java的 |
+      ||
+      not  相当于java的 !
+=cut
+
+$a = true;
+$b = false;
+ 
+print "\$a = $a , \$b = $b\n";
+ 
+$c = ($a and $b);
+print "\$a and \$b = $c\n";
+ 
+$c = ($a  && $b);
+print "\$a && \$b = $c\n";
+ 
+$c = ($a or $b);
+print "\$a or \$b = $c\n";
+ 
+$c = ($a || $b);
+print "\$a || \$b = $c\n";
+ 
+$a = 0;
+$c = not($a);
+print "not(\$a)= $c\n";
 
